d0a0aaf7b386fddebcdf8779ae9251e757fa24ae
Alexander Kern <kern.alex@berkeley.edu>
2019-06-16 14:08:58 -0700

    Set up some example code

---
+++ GoodNews.py
@@ -0,0 +1,16 @@
+from lib.article import Article
+
+
+########################################
+#
+# This is only an example, design before
+# implementation, do not worry about
+# optimization.
+#
+########################################
+
+
+
+if __name__=="__main__":
+    # as an aside, in the end this specific article should give a very bad rating when parsed
+    PVTL = Article("http://www.baystreet.ca/articles/techinsider.aspx?articleid=48609")
---
+++ lib/article.py
@@ -0,0 +1,6 @@
+import requests
+
+class Article:
+
+    def __init__(self, url):
+        self.art = requests.get(url)
